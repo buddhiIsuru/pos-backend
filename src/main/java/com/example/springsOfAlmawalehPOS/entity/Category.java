7ee2208262e779c96439cb4adebb520e210d2774
BuddhiCommercialTP <buddhi@commercialtp.com>
2023-05-18 01:35:02 +0530

    add dependency file

--- src/main/java/com/example/springsOfAlmawalehPOS/entity/Category.java
+++ src/main/java/com/example/springsOfAlmawalehPOS/entity/Category.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import java.sql.Date;
 import java.time.LocalDateTime;
 import java.util.Set;
 
@@ -24,10 +25,10 @@ public class Category {
     private String name;
 
     @CreationTimestamp
-    private LocalDateTime createdAt;
+    private Date createdAt;
 
     @UpdateTimestamp
-    private LocalDateTime updatedAt;
+    private Date updatedAt;
 
     @OneToMany(mappedBy = "category")
     private Set<SubCategory> subCategory;
